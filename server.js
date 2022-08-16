29b68591a76983cd74f2b0b5aeb6b5b6d78ac0ec
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-16 22:47:00 +1000

    updated Server.js

--- server.js
+++ server.js
@@ -1,19 +1,64 @@
-const express=require('express');
-const mysql=require('mysql12');
-var inquirer=require('inquirer');
-const PORT=process.env.PORT || 3000;
-const app= express();
+const mysql = require("mysql2");
+var inquirer = require("inquirer");
+const consTable = require("console.table");
 
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-
-const db=mysql.createConnection({
-    host:'localsost',
-    user:'root',
-    database:'company_db',
-    password:'Sudheer@123'
-
-},
-console.log(`conected to database`)
+//creating mysql connection
+const db = mysql.createConnection(
+  {
+    host: "localsost",
+    user: "root",
+    database: "company_db",
+    password: "Sudheer@123",
+  },
+  console.log(`conected to database`)
 );
 
+db.connect((err) => {
+  if (err) throw err;
+  console.log(`------------------
+    ERROR
+    ---------------------------------`);
+    runEmployees();
+});
+const runEmployees = () => {
+  inquirer
+  .prompt({
+    name: 'options',
+    type: 'list',
+    message: 'What Would You like to do?',
+    choices: ['See All Employees', 'View all titles By Department', 'Add employee', 'Remove employee', 'Update employee role', 'Add Role', 'Add Department', 'Exit']
+  })
+  .then((Choice) => {
+    // console.log("You chose : ", Choice);
+    switch (Choice.options) {
+      case 'See All Employees':
+        //Add all case options
+        readEmployee_Data_func()
+        break
+      case 'View all titles By Department':
+        View_by_department()
+        break
+      case 'Add employee':
+        addEmployee()
+        break
+      case 'Remove employee':
+        deleteEmployee_Data_func()
+        break
+      case 'Update employee role':
+        updateEmployee_Data_func()
+        break
+      case 'Add Role':
+        addRole()
+        break
+      case 'Add Department':
+        addDepartment_func()
+        break
+      case 'Exit':
+        console.log('Thanks for playing');
+        connection.end();
+        break
+      default:
+        console.log('We have run into major issues');
+    }
+  })
+}
