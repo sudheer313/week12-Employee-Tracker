7b3e898536eeb58e52372343693c71dfd18ea499
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-17 21:08:30 +1000

    updated server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,20 +8,20 @@ values
 insert into roles(title,salary,department_id)
 values
 
-('lawyer',1000,1),
+('law_dep Manager',1000,1),
 ('leagal aid',500,1),
-('Charted_account ',2000,2),
+('Accountant Manager ',2000,2),
 ('accountant',1500,2),
 ('sales manager',2500,3),
 ('sales lead',2000,3),
-('engineering head',2500,4),
+('engineering manager',2500,4),
 ('electrical engineer',2000,4);
 
 insert into employee(first_name,last_name,roles_id,manager_id)
 values
 ('narender','kandula',4,null),
 ('Adilaxmi','kandula',5,null),
-('sudheer','kandula',1,null),
-('nihar','kandula',4,null),
+('sudheer','kandula',1,2),
+('nihar','kandula',4,3),
 ('vasanthi','kandula',4,null),
 ('aaradya','kandula',3,null);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const consTable = require("console.table");
 //creating mysql connection
 const db = mysql.createConnection(
   {
-    host: "localsost",
+    host: "localhost",
     user: "root",
     database: "company_db",
     password: "Sudheer@123",
@@ -14,38 +14,46 @@ const db = mysql.createConnection(
 );
 
 db.connect((err) => {
+    
   if (err) throw err;
   console.log(`------------------
-    ERROR
+    
     ---------------------------------`);
-    runEmployees();
+    runEmployee();
 });
-const runEmployees = () => {
+const runEmployee = () => {
   inquirer
   .prompt({
     name: 'options',
     type: 'list',
     message: 'What Would You like to do?',
-    choices: ['See All Employees', 'View all titles By Department', 'Add employee', 'Remove employee', 'Update employee role', 'Add Role', 'Add Department', 'Exit']
+    choices: ['view All Employees',
+    'View all Departments', 
+     'view all roles', 
+     'add department', 
+     'Update employee role', 
+     'Add Role', 
+     'Add Department', 
+     'Exit']
   })
   .then((Choice) => {
     // console.log("You chose : ", Choice);
     switch (Choice.options) {
-      case 'See All Employees':
+      case 'view All Employees':
         //Add all case options
-        readEmployee_Data_func()
+        viewAllEmployees()
         break
-      case 'View all titles By Department':
-        View_by_department()
+      case 'View all Departments':
+        viewDepartments()
         break
-      case 'Add employee':
-        addEmployee()
+      case 'view all roles':
+        viewRoles()
         break
-      case 'Remove employee':
-        deleteEmployee_Data_func()
+      case 'add department':
+        addDepartment()
         break
       case 'Update employee role':
-        updateEmployee_Data_func()
+        updateEmployeeRole_Data_func()
         break
       case 'Add Role':
         addRole()
@@ -54,11 +62,88 @@ const runEmployees = () => {
         addDepartment_func()
         break
       case 'Exit':
-        console.log('Thanks for playing');
+        console.log('Bye Bye');
         connection.end();
         break
       default:
-        console.log('We have run into major issues');
+        console.log(`error`);
     }
+    
   })
 }
+//view employees
+const viewAllEmployees=() =>{
+  console.log('view All Employees data');
+  db.query(`select concat(first_name,last_name) AS FullName from employee `, (err, res) =>
+  {
+      if(err) throw err;
+
+      console.table(res);
+
+      console.log(`
+      ----------------------------------------------------
+      ----------------------------------------------------
+      `);
+
+      runEmployee();
+  });  
+}
+//view departmnet
+const viewDepartments=() =>{
+  console.log('view all deparments');
+  db.query(`select *from department`, (err,res)=>
+  {
+    if(err) throw err;
+    console.table(res);
+    console.log(`
+      ----------------------------------------------------
+      ----------------------------------------------------
+      `);
+      runEmployee();
+  });
+}
+//view roles
+const viewRoles=() =>{
+  console.log('view all roles');
+  db.query(`select roles.id, roles.title AS title, department.name AS department, roles.salary FROM roles INNER JOIN department on roles.department_id=department.id`, (err,res)=>
+  {
+    if(err) throw err;
+    console.table(res);
+    console.log(`
+      ----------------------------------------------------
+      ----------------------------------------------------
+      `);
+      runEmployee();
+  });
+}
+// Add department
+
+const addDepartment=() =>{
+  console.log('add department');
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "newDepartment",
+      message: "What is the new department name ?",
+    },
+  ])
+  .then((answer) => {
+    db.query(
+      `INSERT INTO department (name) VALUES (?)`,
+      [answer.newDepartment],
+      function (err, results) {
+        if (err) {
+          console.log(err);
+        }
+        console.table(results);
+        console.log(`Added ${answer.newDepartment} to the database`);
+      runEmployee();
+    
+  });
+});
+}
+
+
+
+
